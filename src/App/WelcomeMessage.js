ae461c21810609238ed41e39ebc4e9d77d936dc0
turbanboy <hsinghk7@gmail.com>
2021-03-27 16:26:50 -0700

    Add styled Components

---
+++ src/App/WelcomeMessage.js
@@ -0,0 +1,4 @@
+import { Component } from "react";
+export default function Welcome(props) {
+    return <h1>Welcome to The Crypro Dashboard</h1>;
+  }
--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,19 @@
-
 import './App.css';
+import Welcome from './WelcomeMessage';
+import styled, {css} from 'styled-components';
+
+const MyButton = styled.div`
+  color:green;
+
+  ${props => props.primary && css`
+    color: palevioletred;
+  `}
+`
 
 function App() {
   return (
     <div >
-      Hello World
+      <Welcome />
     </div>
   );
 }
