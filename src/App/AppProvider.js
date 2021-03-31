2f397a616c60cae0c35bcfe8671648fe3e116fb0
turbanboy <hsinghk7@gmail.com>
2021-03-31 11:42:53 -0700

    Add cryptocompare api

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+const cc = require('cryptocompare');
 
 export const AppContext = React.createContext();
 
@@ -15,6 +16,16 @@ export class AppProvider extends React.Component{
             confirmFavorites: this.confirmFavorites
 
         }
+    }
+
+
+    componentDidMount = () => {
+        this.fetchCoins();
+    }
+
+    fetchCoins = async () => {
+        let coinList = (await cc.coinList()).Data;
+        this.setState({coinList});
     }
 
     confirmFavorites = () =>{
