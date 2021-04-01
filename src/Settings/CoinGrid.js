341b311314eb7eea94a72b5b3430b72ba9a17b5d
turbanboy <hsinghk7@gmail.com>
2021-04-01 02:12:30 -0700

    Create coingrid

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import styled, {css} from 'styled-components';
 import {AppContext} from '../App/AppProvider';
-
+import {SelectableTile} from '../Shared/Tile';
 export const CoinGridStyled = styled.div`
     display:grid;
     grid-template-columns: repeat(5,1fr);
+    grid-gap: 15px;
 `
 
 
@@ -12,9 +13,9 @@ export default function() {
     return <AppContext.Consumer>
         {({coinList}) => <CoinGridStyled>
             {Object.keys(coinList).map(coinKey => 
-            <div>
+            <SelectableTile>
                 {coinKey}
-            </div>)}
+            </SelectableTile>)}
             </CoinGridStyled>}
     </AppContext.Consumer>
 }
