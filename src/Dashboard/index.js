7ec1591c670e49caae32be597b958434b5b9e269
turbanboy <hsinghk7@gmail.com>
2021-04-09 17:42:21 -0700

    Add Coin Spotlight

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
 import Page from '../Shared/Page';
+import CoinSpotlight from './CoinSpotlight';
 import PriceGrid from './PriceGrid';
 export default function(){
 
+
+const ChartGrid = styled.div`
+
+    display: grid;
+    margin-top: 20px;
+    grid-gap: 15px;
+    grid-template-columns: 1fr 3fr;
+
+`
+
     return <Page name= "dashboard">
         <PriceGrid/>
+          <ChartGrid>
+           <CoinSpotlight/>
+           <div>Chart Goes here</div>
+           </ChartGrid>
         </Page>
 } 
